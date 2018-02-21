63fd914480959b42678214678150b085c211bc6f
Sugigeet <36618625+Sugigeet@users.noreply.github.com>
2018-02-21 10:04:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,3 @@
-console.log('Loaded!');
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight()
-{
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + "px";
-}
-
-img.onclick = function()
-{
-    var interval = setInterval(moveRight, 50);
-};
-
 var button = document.getElementById('counter');
 
 button.onclick = function()
